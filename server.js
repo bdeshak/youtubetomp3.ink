5a69929ec473ed5c3d2f3aa81139a5d528210af7
Eshak <43699326+bdeshak@users.noreply.github.com>
2023-03-26 12:31:56 +0600

    Update server.js

--- server.js
+++ server.js
@@ -5,6 +5,12 @@ const ytdl = require('ytdl-core');
 const app = express();
 const fs = require('fs');
 app.use('/', express('./'));
+app.all('*', function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
 
 app.listen(port, () => {
     console.log("Server listening on port=>>" + port);
