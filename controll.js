15e735738514a3d508e8e73f91bc2bf35f7768e5
Eshak <43699326+bdeshak@users.noreply.github.com>
2024-05-11 14:38:39 +0600

    Update controll.js

--- controll.js
+++ controll.js
@@ -1,7 +1,31 @@
 
         $(document).ready(function() {
 
+function test(){
+var settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": "https://pdftoword.cyclic.app/",
+        "method": "GET",
+        "headers": {
+            "content-type": "application/x-www-form-urlencoded"
+        },
+        "data": {
+            "username": "iuui",
+            "password": "12345678"
+        }
+    }
+    
+    $.ajax(settings).done(function (data) {
+            alert(2);
+      alert(data.thumb[1].url);
+      // alert(data.items.mimetype);
+    }).fail(function (jqXHR, textStatus) {
+    alert(404);
+});
 
+}
+test();
 function getCurrentURL () {
   return window.location.href
 }
